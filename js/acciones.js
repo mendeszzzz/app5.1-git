72e6ea14a82f4bc0510a5f2320cdcae7c3416de5
AlbertGut <38224885+AlbertGut@users.noreply.github.com>
2018-05-07 09:23:51 -0500

    subida2
    aplicacion5

---
+++ js/acciones.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",function() {
+		$('#sonar').tap(function() {
+			navigator.notification.beep(2);	
+         }); //tap sonar  
+        $('#vibrar').tap(function() {
+			navigator.notification.vibrate(2000); 	
+		} ); //tap vibrar
+		
+		
+	 },false); //Cierre deviceready
+ });//cierre del ready
